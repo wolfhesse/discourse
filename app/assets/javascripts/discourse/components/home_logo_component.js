b8fdb771712a9c0446ac1e196bd7c62900cb84df
wolfhesse <wolfgang.schuessel@gmail.com>
2014-03-24 19:51:37 +0100

    wp

--- app/assets/javascripts/discourse/components/home_logo_component.js
+++ app/assets/javascripts/discourse/components/home_logo_component.js
@@ -1,23 +1,29 @@
 Discourse.HomeLogoComponent = Ember.Component.extend({
-  classNames: ["title"],
+    classNames: ["title"],
 
-  linkUrl: function() {
-    return Discourse.getURL("/");
-  }.property(),
+    linkUrl: function () {
+        return Discourse.getURL("/");
+    }.property(),
 
-  showSmallLogo: function() {
-    return !Discourse.Mobile.mobileView && this.get("minimized");
-  }.property("minimized"),
+    // hack r20140320
+    whiteCanvas: function () {
+        return Discourse.getUrl("/");
+    }.property(),
+    // eo-hack
 
-  smallLogoUrl: function() {
-    return Discourse.SiteSettings.logo_small_url;
-  }.property(),
+    showSmallLogo: function () {
+        return !Discourse.Mobile.mobileView && this.get("minimized");
+    }.property("minimized"),
 
-  bigLogoUrl: function() {
-    return Discourse.SiteSettings.logo_url;
-  }.property(),
+    smallLogoUrl: function () {
+        return Discourse.SiteSettings.logo_small_url;
+    }.property(),
 
-  title: function() {
-    return Discourse.SiteSettings.title;
-  }.property()
+    bigLogoUrl: function () {
+        return Discourse.SiteSettings.logo_url;
+    }.property(),
+
+    title: function () {
+        return Discourse.SiteSettings.title;
+    }.property()
 });
